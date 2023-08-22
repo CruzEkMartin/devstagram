7de372af8a924b4d7ebe7410558a92ecf424e2b7
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-08-22 17:01:38 -0500

    se comieza la implementacion de seguridad cross site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
             <nav>
                 <a class="font-bold uppercase text-gray-600" href="">Login</a>
-                <a class="font-bold uppercase text-gray-600" href="">Crear Cuenta</a>
+                <a class="font-bold uppercase text-gray-600" href="/crear-cuenta">Crear Cuenta</a>
             </nav>
 
         </div>
@@ -39,7 +39,7 @@
     </main>
 
 
-    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+    <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
 DevStagram - Todos los derechos reservados {{ now()->year }}
     </footer>
 
